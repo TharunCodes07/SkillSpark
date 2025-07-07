4fac0a5823b00d7367f0064a4af5b4328125c71d
TharunCodes07 <senthietk@gmail.com>
2025-07-08 00:33:03 +0530

    feat: Intial Commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,22 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
-import "../../global.css";
-import React from "react";
-import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-export default function RootLayout() {
+import { useColorScheme } from "@/components/useColorScheme";
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
   return (
-    <React.Fragment>
-      <StatusBar style="auto" />
-      <Stack />
-    </React.Fragment>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
